89c0fc76191f0fdc106ff7dc4aa298b4094d1239
modalsoul0226 <modalsoul0226@gmail.com>
2018-06-08 00:35:15 +0800

    Corrected UVa133.

--- UVa_Problems/Chapter4/UVa133_The_Dole_Queue.c
+++ UVa_Problems/Chapter4/UVa133_The_Dole_Queue.c
@@ -10,31 +10,40 @@ int main() {
 
     while (scanf("%d%d%d", &n, &step_a, &step_b) == 3 && n) {
         // Initialization.
-        int group[n + 1]; for (int i = 0; i <= n; i++) group[i] = 1;
-        int left = n, idx_a = 1, idx_b = n, comp_b = 1;
+        int group[n]; for (int i = 0; i < n; i++) group[i] = 1;
+        int left = n, idx_a = 0, idx_b = n - 1, comp_b = 0;
         
         while (left) {
-            if (left < n) printf(",");
-            printf("comp_b: %d idx_b:%d\n idx_a:%d", comp_b, idx_b, idx_a);
-            idx_a = (idx_a + step_a - 1) % n;
-            comp_b = (comp_b + step_b - 1) % n;
-            idx_b = n - comp_b + 1;
-
+            // Proceed to the next available person.
             while (!group[idx_a]) idx_a = (idx_a + 1) % n;
             while (!group[idx_b])
-                { comp_b = (comp_b + 1) % n; idx_b = n - comp_b + 1; }
+                { comp_b = (comp_b + 1) % n; idx_b = n - 1 - comp_b; }
+            if (left < n) printf(",");
+            // printf("comp_b: %d idx_b:%d idx_a:%d\n", comp_b, idx_b, idx_a);
             
+            // A selects the next person.
+            for (int i = 0; i < step_a - 1; i++) {
+                idx_a = (idx_a + 1) % n;
+                while (!group[idx_a]) idx_a = (idx_a + 1) % n;
+            }
+
+            // B selects the next person.
+            for (int i = 0; i < step_b - 1; i++) {
+                comp_b = (comp_b + 1) % n;
+                idx_b = n - 1 - comp_b;
+                while (!group[idx_b])
+                    { comp_b = (comp_b + 1) % n; idx_b = n - 1 - comp_b; }
+            }
+            // printf("idx_a:%d idx_b:%d\n", idx_a, idx_b);
+
+            // Hand out the dole.
             if (idx_a == idx_b)
-                { left--; group[idx_a] = 0; printf("%3d", idx_a); }
+                { left--; group[idx_a] = 0; printf("%3d", idx_a + 1); }
             else {
                 left -= 2;
                 group[idx_a] = 0; group[idx_b] = 0;
-                printf("%3d", idx_a); printf("%3d", idx_b);
+                printf("%3d", idx_a + 1); printf("%3d", idx_b + 1);
             }
-
-            while (!group[idx_a]) idx_a = (idx_a + 1) % n;
-            while (!group[idx_b])
-                { comp_b = (comp_b + 1) % n; idx_b = n - comp_b + 1; }
         }
         printf("\n");
     }
