f892531c98a1a21228c359dffa715a84d02a957f
modalsoul0226 <modalsoul0226@gmail.com>
2018-06-24 18:59:50 +0800

    Added another solution to UVa11899.

--- UVa_Problems/Chapter6/UVa11988_Broken_Keyboard.cpp
+++ UVa_Problems/Chapter6/UVa11988_Broken_Keyboard.cpp
@@ -1,5 +1,10 @@
 #include <cstdio>
 #include <cstring>
+#include <list>
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 const int maxn = 1e5 + 5;
 int last, curr, next[maxn];
@@ -10,27 +15,43 @@ int main() {
     freopen("./UVa11988.fin", "r", stdin);
 #endif
 
-    while (scanf("%s", s + 1) == 1) {
-        auto n = (int)strlen(s + 1);
-        last = curr = 0;
-        next[0] = 0;
+    string s;
+    while (getline(cin, s)) {
+        list<char> chars;
+        auto curr = chars.begin();
 
-        for (int i = 1; i <= n ; i++) {
-            char ch = s[i];
-            if (ch == '[') curr = 0;
-            else if (ch == ']') curr = last;
-            else {
-                next[i] = next[curr];
-                next[curr] = i;
-                if (curr == last) last = i;
-                curr = i;
-            }
+        for (char i: s) {
+            if (i == '[') curr = chars.begin();
+            else if (i == ']') curr = chars.end();
+            else chars.insert(curr, i);
         }
 
-        for (int i = next[0]; i != 0; i = next[i])
-            printf("%c", s[i]);
-        printf("\n");
+        for (auto c: chars)
+            cout << c;
+        cout << endl;
     }
+
+//    while (scanf("%s", s + 1) == 1) {
+//        auto n = (int)strlen(s + 1);
+//        last = curr = 0;
+//        next[0] = 0;
+//
+//        for (int i = 1; i <= n ; i++) {
+//            char ch = s[i];
+//            if (ch == '[') curr = 0;
+//            else if (ch == ']') curr = last;
+//            else {
+//                next[i] = next[curr];
+//                next[curr] = i;
+//                if (curr == last) last = i;
+//                curr = i;
+//            }
+//        }
+//
+//        for (int i = next[0]; i != 0; i = next[i])
+//            printf("%c", s[i]);
+//        printf("\n");
+//    }
 
     return 0;
 }
